ef21a6d3ebe23394375bcab613354a6468e74c1d
wy6 <wy1636688@163.com>
2018-03-03 16:20:15 +0800

    前台页面（数据未分页）

--- src/main/java/com/moutai/nss/web/UserController.java
+++ src/main/java/com/moutai/nss/web/UserController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.jws.WebParam;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description:
  * @Auther: LEE
@@ -22,7 +26,7 @@ public class UserController extends BaseController {
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ModelAndView userLogout() {
-        return null;
+        return new ModelAndView("");
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -30,4 +34,33 @@ public class UserController extends BaseController {
         return null;
     }
 
+    @RequestMapping(value = "/index1", method = RequestMethod.GET)
+    public ModelAndView userIndex() {
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/addSalerecord", method = RequestMethod.GET)
+    public ModelAndView userAddSalerecord() {
+
+        ModelAndView mv = new ModelAndView("addSalerecord");
+        mv.addObject("actList", test.findAllAct());
+        return mv;
+    }
+
+    @RequestMapping(value = "/actList", method = RequestMethod.GET)
+    public ModelAndView userActList() {
+        return new ModelAndView("actList");
+    }
+
+    @RequestMapping(value = "/signIn", method = RequestMethod.GET)
+    public ModelAndView userSignIn() {
+        return new ModelAndView("signIn");
+    }
+
+    @RequestMapping(value = "/signUp", method = RequestMethod.GET)
+    public ModelAndView userSignUp() {
+        ModelAndView mv = new ModelAndView("signUp");
+        mv.addObject("storeList", test.findAllStore());
+        return mv;
+    }
 }
--- src/test/java/com/moutai/nss/NssApplicationTests.java
+++ src/test/java/com/moutai/nss/NssApplicationTests.java
@@ -1,5 +1,6 @@
 package com.moutai.nss;
 
+import com.moutai.nss.web.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class NssApplicationTests {
 
 	@Test
 	public void contextLoads() {
+//		test.findAllStore();
 	}
 
 }
